851c81f450230db7c52f0a8c213f6641070cd2ee
apple <448403864@qq.com>
2019-03-21 16:00:20 +0800

    首页动画完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,41 @@
 <template>
-	<header class="bg">
-		 <section class="header-content">
-			<svg class="icon" aria-hidden="true" style="font-size: 200px; color: #fff">
-				<use xlink:href="#icon-nvrenbang"></use>
-			</svg>
-      <h1 class="header-title">欢迎来到SS女人咖啡馆</h1>
-      <h3 class="header-subtitle">轻奢 优雅 宁静 从容</h3>
-			<v-btn color="info">Get started today</v-btn>
-    </section>
-	</header>
+<header class="bg">
+<section class="header-content">
+<svg class="rocky-dashed icon animate-pop-in" aria-hidden="true" style="font-size: 200px; color: #fff">
+<use xlink:href="#icon-nvrenbang"></use>
+</svg>
+<h1 class="header-title animate-pop-in">欢迎来到SS女人咖啡馆</h1>
+<h3 class="header-subtitle animate-pop-in">轻奢 优雅 宁静 从容</h3>
+<v-btn color="info" class=" animate-pop-in header-button">Get started today</v-btn>
+</section>
+<section class="header-down-arrow">
+<img :src="arrow" width="100">
+</section>
+</header>
 </template>
 
 <script>
-
+import arrow from '@/assets/image/arrow.png';
 export default {
+	data() {
+		return {
+			arrow,
+		}
+	},
+	mounted() {
+	},
 }
 </script>
 
-<style lang="stylus" scoped>
-header 
-	margin: -16px 0 0 -16px;
+<style lang="less" scoped>
+header {
+	position: absolute;
+	top: 0;
+	left: 0;
+
+
 	align-items: center;
-	background: #333;;
+	background: #333;
 	display: flex;
 	font-size: 18px;
 	height: 100vh;
@@ -29,24 +43,27 @@ header
 	justify-content: center;
 	overflow: hidden;
 	perspective: 100px;
-	position: relative;
 	text-align: center;
 	transform-style: preserve-3d;
-	&:before
-		animation: fade-slide-down 2s .5s cubic-bezier(0, 0.5, 0, 1) forwards;
-		background: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,.8)),url("../assets/image/home.jpg") no-repeat bottom; 
+
+	&:before {
+		animation: no-transform 2s .5s cubic-bezier(0, 0.5, 0, 1) forwards;
+		background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8)), url("../assets/image/home.jpg") no-repeat bottom;
 		background-size: cover;
 		content: "";
+		transform: translateY(-4rem);
 		position: absolute;
 		top: 0;
 		right: 0;
 		bottom: 0;
 		left: 0;
 		z-index: -1;
+	}
 
-
-	&:after 
-		background: #F9FCFF;;
+	&:after {
+		animation: rotate-up .5s .5s cubic-bezier(0, 0.5, 0, 1) forwards;
+		background: #F9FCFF;
+		;
 		content: "";
 		height: 40rem;
 		left: -5%;
@@ -54,28 +71,85 @@ header
 		right: -5%;
 		top: 90%;
 		transform-origin: 0 0;
-		transform: rotate(-4deg);
+	}
 
-.header-title, .header-subtitle
-	color: #fff;
-	margin-bottom: 5rem;
-	text-transform: uppercase;
+	.headerBase {
+		margin-bottom: 5rem;
+		text-transform: uppercase;
+	}
 
-.header-button
-	transorm: translateZ(.1px);
-	position: relative;
-	z-index: 10;
+	.rocky-dashed {
+		animation-delay: 1s;
+	}
 
-@keyframes fade-slide-down {
-  0% {
-    opacity: 0;
-    transform: translateY(-4rem);
-  }
-  100% {
-    opacity: 1;
-    transform: none;
-  }
+	.header-subtitle {
+		.headerBase;
+		animation-delay: 1.2s;
+	}
+
+	.header-title {
+		.headerBase;
+		color: #fff;
+		animation-delay: 1.4s;
+	}
+
+	.header-button {
+		animation-delay: 1.6s;
+		transorm: translateZ(.1px);
+		position: relative;
+		z-index: 10;
+	}
 }
-	
-</style>
 
+
+.animate-pop-in {
+	animation: no-transform .6s cubic-bezier(0, 0.9, 0.3, 1.2) forwards;
+	opacity: 0;
+	transform: translateY(-4rem) scale(.8);
+}
+.header-down-arrow {
+	position: absolute;
+	bottom: 4vh;
+	left: 0;
+	right: 0;
+	text-align: center;
+	z-index: 10;
+	img {
+		animation: no-transform 1s 1s ease-out forwards,
+		pulse 2s 3s ease-out infinite;
+		opacity: 0;
+		transform: translateY(4rem);
+	}
+}
+
+@keyframes no-transform {
+	100% {
+		opacity: 1;
+		transform: none;
+	}
+}
+
+/* 背景旋转出现 */
+@keyframes rotate-up {
+	100% {
+		transform: rotateZ(-4deg);
+	}
+}
+
+
+/* 箭头心跳动画 */
+@keyframes pulse {
+	0% {
+		opacity: 1;
+		transform: none;
+	}
+	50% {
+		opacity: .8;
+		transform: scale(.8);
+	}
+	100% {
+		opacity: 1;
+		transform: none;
+	}
+}
+</style>
