d7d0d6c216e352b6c203c202bc42a1d07e9d09da
apple <448403864@qq.com>
2019-03-21 12:56:50 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<header>
+		 <section class="header-content">
+      <!-- <img src="images/rocky-dashed.svg" class="rocky-dashed"> -->
+      <h1 class="header-title">Your awesome landing page</h1>
+      <h3 class="header-subtitle">A useful start for your projects</h3>
+      <p class="header-button"><a href="#calls-to-action" class="button">Get started today</a></p>
+    </section>
+	</header>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+header 
+	align-items: center;
+</style>
+
